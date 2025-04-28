f41c798541f38f18860135711d1532b241e08587
Dingwoonee <slm0523@naver.com>
2025-04-28 16:56:45 +0900

    [feat] 입양 동물 검색 조건 입력창 디자인 및 구현

--- src/components/pet/AddressModal.jsx
+++ src/components/pet/AddressModal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./AddressModal.css";
+import PrimaryButton from "../common/PrimaryButton";
 
 function AddressModal({ onClose }) {
   return (
@@ -7,7 +8,7 @@ function AddressModal({ onClose }) {
       <div className="address-modal">
         <h2>지역 조건 추가</h2>
         <p>여기에 지역 조건 입력 UI가 들어갑니다.</p>
-        <button className="close-btn" onClick={onClose}>닫기</button>
+        <PrimaryButton className="close-btn" onClick={onClose}>닫기</PrimaryButton>
       </div>
     </div>
   );
