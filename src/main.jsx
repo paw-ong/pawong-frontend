43161d472ea667c4c1282175df3d40063db68013
Sungyeon <csy357@gmail.com>
2025-04-30 10:36:17 +0900

    [chore] main.jsx 주석

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext.jsx'
+import { AuthProvider } from './contexts/AuthContext.jsx'     // 하위 컴포넌트에서 전역 상태 사용 가능
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
