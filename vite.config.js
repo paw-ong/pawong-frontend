8567484fc2a28b6ee7d8af068082d6e243ea90c8
Sungyeon <csy357@gmail.com>
2025-05-06 20:15:12 +0900

    [fix] Nginx 설정 적용 이후 Axios endpoint 통일 (#25)
    * [feat] 유기동물 페이지네이션 조회 api endpoint 변경
    
    * [fix] 유기동물 검색 api endpoint 변경
    
    * [fix] 유기동물 검색 api endpoint 변경 (search)
    
    * [fix] 유기동물 추천리스트 api endpoint 변경
    
    * [chore] mypage css 쉼표 수정
    
    * [fix] 로그인 상태가 아니어도 로그인 페이지로 이동하지 않도록 수정
    
    * [refactor] vite proxy code 삭제
    
    * [chore] 주석 제거
    
    * [chore] delete .env file
    
    * [fix] 유기동물 찜 목록 api endpoint 변경
    
    * [fix] 마이페이지에서 새로고침해도 로그인 상태 풀리지 않도록 수정
    
    * [build] package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -7,19 +7,5 @@ export default defineConfig({
   server: {
     port: 5004,
     host: true,     // 또는 host: '0.0.0.0'
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-      '/oauth2': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-      '/login/oauth2': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      }
-    }
   }
 })
